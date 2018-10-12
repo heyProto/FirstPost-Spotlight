08a5a26b686e7d44da1f0e630e2dbe0e9124d28e
Shivangi Tikekar <shivz@live.in>
2018-10-12 15:36:43 +0530

    responsiveve cover

--- src/js/cover.js
+++ src/js/cover.js
@@ -1,35 +1,49 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import '../css/cover.css';
+
+class SpanRenderer extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <span>{JSON.stringify(this.props)}</span>;
+  }
+}
 
 export default class MeTooCover extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      data: [{
+        "title": "Placeholder",
+        "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tempus ut nisi vitae rhoncus. Nulla tempor commodo libero ac efficitur. Aenean porta justo in porttitor porta. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tempus ut nisi vitae rhoncus. Nulla tempor commodo libero ac efficitur. Aenean porta justo in porttitor porta."
+      }]
+    }
   }
 
   exportData() {
     return this.props.selector.getBoundingClientRect();
   }
 
-  renderCol4() {
+  renderCover() {
+    let data = this.state.data[0]
+    let num = this.props.dataSize
+    console.log(num)
+    let numArray = num.toString().split('')
+    console.log(numArray)
       return (
-        <div id="protograph_div" className="protograph-col4-mode">
-          In progress
-        </div>
-      );
-  }
-
-  renderCol16() {
-      return (
-        <div id="protograph_div" className="protograph-col16-mode">
-        
+        <div id="protograph_div" className="cover-container">
           <div className="stats-div">
-            <div className="title">placeholder</div>
-            <div className="desc">Lorem ipsum dolor sit amet, consectetur <b>adipiscing</b> elit. Nullam tempus ut nisi vitae rhoncus. Nulla tempor commodo libero ac efficitur. Aenean porta justo in porttitor porta. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tempus ut nisi vitae rhoncus. Nulla tempor commodo <b>libero</b> ac efficitur. Aenean porta justo in porttitor porta.</div>
+            <div className="title">{data.title}</div>
+            <div className="desc">{data.desc}</div>
             <div className="flip-num-container">
             <div className="flip-counter">
-              <div className="flip-num-bg"><div className="flip-num">3</div></div>
-              <div className="flip-num-bg"><div className="flip-num">2</div></div>
-              <div className="flip-num-bg"><div className="flip-num">8</div></div>
+              {numArray.map(x => {return (
+                  <div className="flip-num-bg"><div className="flip-num">{x}</div></div>
+                )})}
             </div>
               <div className="flip-counter-caption">incidents reported</div>
             </div>
@@ -48,6 +62,6 @@ export default class MeTooCover extends React.Component {
   }
 
   render() {
-        return this.renderCol16();
+    return this.renderCover()
   }
 }
